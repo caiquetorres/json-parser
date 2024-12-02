f25e564b7ff3a0a828d64df2209f54e4604c27ee
Caique Torres <caiquet.bastos@gmail.com>
2024-12-01 21:50:33 -0300

    feat: improve the json ouput

--- parser/run.go
+++ parser/run.go
@@ -7,6 +7,7 @@ import (
 
 func Run(r io.ReadSeeker) error {
 	ts := newTokenStream(r)
+	var toks []token
 	for {
 		tok, err := ts.next()
 		if err != nil {
@@ -16,7 +17,15 @@ func Run(r io.ReadSeeker) error {
 				return err
 			}
 		}
-		fmt.Println(tok.string())
+		toks = append(toks, tok)
 	}
+	for _, tok := range toks {
+		txt, err := tok.textContent(r)
+		if err != nil {
+			return err
+		}
+		fmt.Print(txt)
+	}
+	fmt.Println()
 	return nil
 }
