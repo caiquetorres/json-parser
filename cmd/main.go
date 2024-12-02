4c4e63a5f1ef33dbdeb3fd8771156da2f518a8cc
Caique Torres <caiquet.bastos@gmail.com>
2024-12-02 17:34:48 -0300

    feat: enhance file handling and output formatting

--- cmd/main.go
+++ cmd/main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"flag"
 	"fmt"
+	"io"
 	"log"
 	"os"
 
@@ -9,15 +11,37 @@ import (
 )
 
 func main() {
-	file, err := os.Open("test.json")
-	if err != nil {
-		log.Fatal(err)
+	flag.Parse()
+	paths := flag.Args()
+	type reader struct {
+		id string
+		r  io.Reader
 	}
-	err = jsonparser.Parse(file)
-	if err != nil {
-		fmt.Println("invalid json")
-		log.Fatal(err)
+	var readers []reader
+	if len(paths) == 0 {
+		readers = append(readers, reader{
+			id: "stdin",
+			r:  os.Stdin,
+		})
 	} else {
-		fmt.Println("valid json")
+		for _, path := range paths {
+			file, err := os.Open(path)
+			if err != nil {
+				log.Fatal(err)
+			}
+			readers = append(readers, reader{
+				id: path,
+				r:  file,
+			})
+		}
+	}
+	for _, reader := range readers {
+		err := jsonparser.Parse(reader.r)
+		isValid := err == nil
+		if isValid {
+			fmt.Println(fmt.Sprintf("%s valid", reader.id))
+		} else {
+			fmt.Println(fmt.Sprintf("%s invalid", reader.id))
+		}
 	}
 }
